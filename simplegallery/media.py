099328094ddef6dae88011d7a6de5ce6ab1c760e
George V. Kouryachy (Fr. Br. George) <george@altlinux.org>
2023-08-02 12:15:07 +0300

    Drop old (?) ANTIALIAS renderer

--- simplegallery/media.py
+++ simplegallery/media.py
@@ -65,7 +65,7 @@ def create_image_thumbnail(image_path, thumbnail_path, height):
         image = rotate_image_by_orientation(image)
 
     thumbnail_size = get_thumbnail_size(image.size, height)
-    image = image.resize(thumbnail_size, Image.ANTIALIAS)
+    image = image.resize(thumbnail_size)
 
     # Convert to RGB if needed
     if image.mode != "RGB":
